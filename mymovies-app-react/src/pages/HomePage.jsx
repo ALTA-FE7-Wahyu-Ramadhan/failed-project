f3f9588340f8e9e9d87948285591eb71a89aa59e
wramadhan <wahyuramadhan0549@gmail.com>
2022-07-18 12:59:48 +0700

    masih progress task 2

--- mymovies-app-react/src/pages/HomePage.jsx
+++ mymovies-app-react/src/pages/HomePage.jsx
@@ -1,9 +1,5 @@
 import Header from "../components/Header"; 
 import Card from "../components/Card";
-import Batman1 from "../img/batmanBegins.jpg";
-import Batman2 from '../img/batmanNew.jpg';
-import Batman3 from '../img/batmanSuperman.jpg';
-import axios from 'axios'
 
 const HomePage =()=> {
 
@@ -11,7 +7,8 @@ const HomePage =()=> {
       {id:1, gambar:{Batman1}, title: 'Batman Begins', detail: 'Menceritakan tentang kehidupan batman yang kelam'},
       {id:2, gambar:{Batman2}, title: 'Batman 2022', detail: 'Merupakan cerita lanjutan dari batman vs superman'},
       {id:3, gambar:{Batman3}, title: 'Batman vs Superman', detail: 'Batman kalah melawan superman dan superwahyoe'},
-    ]
+      ]
+
 
     return (
       <>
